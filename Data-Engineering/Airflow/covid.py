16c4120d40301208478bbfe09b52a95fe3217732
Raviteja Panugundla <63529934+tarunratan@users.noreply.github.com>
2024-04-25 14:15:55 +0530

    Create covid.py

---
+++ Data-Engineering/Airflow/covid.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+with DAG(dag_id="my_dag", start_date=datetime(2024, 4, 25)) as dag:
+
+    # Task to download data
+    download_data = BashOperator(task_id="download_data", bash_command="wget https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv -O data.csv")
+
+    # Task to process data (replace with your processing logic)
+    process_data = BashOperator(task_id="process_data", bash_command="cut -d ',' -f 1,4 data.csv > processed_data.csv")
+
+    # Define the execution order
+    download_data >> process_data
