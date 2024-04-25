a9b8c4fae4823351f62e6eba4199952d55dede07
Raviteja Panugundla <63529934+tarunratan@users.noreply.github.com>
2024-04-25 14:16:12 +0530

    Update covid.py

--- Data-Engineering/Airflow/covid.py
+++ Data-Engineering/Airflow/covid.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
-with DAG(dag_id="my_dag", start_date=datetime(2024, 4, 25)) as dag:
+with DAG(dag_id="covid_dag", start_date=datetime(2024, 4, 25)) as dag:
 
     # Task to download data
     download_data = BashOperator(task_id="download_data", bash_command="wget https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv -O data.csv")
