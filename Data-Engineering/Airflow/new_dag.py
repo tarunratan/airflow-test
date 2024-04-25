717cb7a0b43bc817d6ab07b526d60b97bfc09ca6
Raviteja Panugundla <63529934+tarunratan@users.noreply.github.com>
2024-04-25 12:43:54 +0530

    Update new_dag.py

--- Data-Engineering/Airflow/new_dag.py
+++ Data-Engineering/Airflow/new_dag.py
@@ -42,3 +42,6 @@ with DAG("my_dag", start_date=datetime(2021, 1, 1), schedule_interval="@daily", 
             task_id="inaccurate",
             bash_command="echo 'inaccurate'"
       )
+
+      [training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
+      
